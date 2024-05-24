0ff665178cb32fa621fb59ef85bf30983bb360d3
Bifabig <biftubig@gmail.com>
2024-05-24 08:26:15 +0300

    update tailwind css

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,26 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*',
+    './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      colors: {
-        highlight: '#663b86',
-        primary: '#156ff7',
-      },
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ],
-};
+    require('@tailwindcss/container-queries'),
+  ]
+}
