2cab91670332a15a9915628b6c13dc6519b55ca6
Bifabig <biftubig@gmail.com>
2024-05-24 12:31:11 +0300

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_22_225946) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_23_225946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_22_225946) do
 
   create_table "queries", force: :cascade do |t|
     t.string "search_term"
-    t.datetime "searched_at", default: -> { "CURRENT_TIMESTAMP" }
+    t.datetime "searched_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
